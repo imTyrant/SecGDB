e8b623e5cc6772e3621c6d45911353ea06596682
imTyrant <cd2235823998@gmail.com>
2019-01-17 10:13:10 +0800

    Add flags used during tranfering mpz_class to  string.

--- include/global.h
+++ include/global.h
@@ -6,10 +6,6 @@ extern "C"
 {
 #endif
 
-#define MAX_GGM_DEPTH 16
-
-typedef unsigned char BYTE;
-
 #ifdef SECURITY_LEVEL_128
 
 #define SECURITY_LEVEL 128
@@ -21,6 +17,21 @@ typedef unsigned char BYTE;
 #define KEY_SIZE 32
 
 #endif
+
+#define WRAPPING_GMP_EXPORT_FORMAT_ORDER     1   // The order is most significant word first
+#define WRAPPING_GMP_EXPORT_FORMAT_SIZE      1   // The byte that a word represent
+#define WRAPPING_GMP_EXPORT_FORMAT_ENDIAN    1   // For uniform use most significant byte first
+#define WRAPPING_GMP_EXPORT_FORMAT_NAIL      0   // Asked to be 0
+
+#define MAX_GGM_DEPTH 20
+
+typedef unsigned char BYTE;
+
+#define SEC_GDB_SIMPLE_MODE
+
+#define SEC_GDB_WITHOUT_ENCRYPTION
+
+void log_memory(const void* ptr, size_t size);
 
 #ifdef __cplusplus
 }
